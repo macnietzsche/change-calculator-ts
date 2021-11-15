94fbda318f9305eda2763cf8821a2fdf247fd92e
macnietzsche <mbvillegas94@outlook.com>
2021-11-15 16:15:37 -0500

    REFACTOR: Has enough change. TDD Red

--- src/services/change-calculator.ts
+++ src/services/change-calculator.ts
@@ -11,9 +11,11 @@ export const calculateCashAmount = (currentCash: ICash | null): number => {
 };
 
 export const processChange = (
-  currentCash: ICash,
+  currentCash: ICash | null,
   requiredValue: number
 ): ICash | null => {
+  if (!currentCash) return null;
+
   const numberOfBills = Object.values(currentCash);
   const billDenominations = Object.keys(currentCash).map((item) =>
     parseInt(item)
@@ -45,6 +47,24 @@ export const processChange = (
   return null;
 };
 
+const INITIAL_NUMBER_OF_BILLS = { 25: 0, 50: 0, 100: 0 } as ICash;
+const TICKET_COST = 25;
 export const hasEnoughChange = (paymentsReceived: number[]): boolean => {
-  return false;
+  let currentCash: ICash | null = INITIAL_NUMBER_OF_BILLS;
+  const validDenominations = Object.keys(INITIAL_NUMBER_OF_BILLS).map(
+    (denonimation) => parseInt(denonimation)
+  );
+  for (const payment of paymentsReceived) {
+    if (!validDenominations.includes(payment)) {
+      return false;
+    }
+
+    const change = payment - TICKET_COST;
+    currentCash = processChange(currentCash, change);
+    if (!currentCash) {
+      return false;
+    }
+    currentCash[payment] += 1;
+  }
+  return true;
 };
